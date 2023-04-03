c6a8ee7cbf265ea706c986c8eb45b362fa13ef88
Max Ward <ward.maximiliano@gmail.com>
2023-04-03 14:17:20 -0300

    Initial Commit

---
+++ SwitChat/SwitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitChatApp.swift
+//  SwitChat
+//
+//  Created by Max Ward on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
